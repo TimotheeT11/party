5cb7e8fcffd001e77c952a69ed31ce515c627c0c
Timothee Thomance <timotheet11@gmail.com>
2025-05-07 11:26:38 -0400

    getEvents function added

--- index.js
+++ index.js
@@ -3,4 +3,17 @@ const COHORT = "/2504-FTB-EB-WEB-FT";
 const RESOURCE = "/events";
 const API = BASE + COHORT + RESOURCE;
 
-console.log(API);
+const events = [];
+
+async function getEvents() {
+  try {
+    const response = await fetch(API);
+    const json = await response.json();
+    const events = json.data;
+    console.log(events);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+getEvents();
