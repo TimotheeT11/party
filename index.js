bd34428017bfe096d6b3fa9a15817227b7c8a978
Timothee Thomance <timotheet11@gmail.com>
2025-05-07 11:10:31 -0400

    api link added

---
+++ index.js
@@ -0,0 +1,6 @@
+const BASE = "https://fsa-crud-2aa9294fe819.herokuapp.com/api";
+const COHORT = "/2504-FTB-EB-WEB-FT";
+const RESOURCE = "/events";
+const API = BASE + COHORT + RESOURCE;
+
+console.log(API);
